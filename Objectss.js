d5fbe049316060b770949087c99036e6cb3ae011
Vaishnavi Kulkarni <55730110+vaishnavi4549@users.noreply.github.com>
2025-07-26 14:59:13 +0530

    Update Objectss.js

--- Objectss.js
+++ Objectss.js
@@ -14,17 +14,17 @@ const func = (function (a){
     delete a;
     return a;
 })(5);
-console.log(func) //here it wll not delete the a it will simply return 5
-//as a is the local variable ofr inner func
-//delete willl be used to delete properties of objs not local variable
+console.log(func) //here it will not delete the a, it will simply return 5
+//as a is the local variable of ithe nner function
+//delete will be used to delete properties of objs, not local variables
 
 //Dynamic Key value to Object
 const property = 'firstname'
 const name = 'test';
 const user = {
-    property:name, //gives propet=test
+    property: name, //gives property test
     // we want firstname=test for that dynamic
-    [property]:name
+    [property]: name
 }
 
 //print all keys ad values of object
@@ -40,7 +40,7 @@ const obj = {
     a:'three'
 }
 console.log(obj) // a:'three' b:'two'
-//if we have 2 keys with he same name if will replace the value with new value for the irts key found
+//if we have 2 keys with he same name, if will replace the value with the new value for the first key found
 
 //QUE 2
 //create function multiplyByTwo(obj) that multiplies all numeric property values of nums by 2
@@ -68,9 +68,9 @@ a[b]=123;
 a[c]=456
 
 console.log(a[b]); //456 is the output :(
-//a[b] -> b can not be assigned as a key to a obj as it's a obj itself it counld have been asiigend if b would be a string -> "[obj,obj]"=123
+//a[b] -> b can not be assigned as a key to a object as it's a object itself, it could have been assigned if b were a string -> "[obj, obj]"=123
 //a[c] -> again c itself is a obj -> [obj,obj]=456 so the value got overwritten 
-//so a will have on eproperty [obj,obj] = 456
+//so a will have on eproperty [obj, obj] = 456
 //if we access a[b] or a[c] - will get 456
 
 //QUE 4
@@ -86,7 +86,7 @@ console.log(clone.name) //gives undefined
 clone =JSON.parse(user) //convert again into obj
 console.log(clone.name) //test
 
-//usecase -> storing data into local storage: setItems-> as we cannot store obj direcly to local storage
+//usecase -> storing data into local storage: setItems-> as we cannot store obj directly to local storage
 localStorage.setItem(test,clone)
 localStorage.setItem(test,user) 
 
@@ -95,17 +95,17 @@ localStorage.getItem(test)//[obj,obj]
 
 //QUE 5
 console.log([..."abcde"]) 
-//output-> it will spread all the characters into array of Charactrs ['a','b']
+//output-> it will spread all the characters into array of Characters ['a','b']
 //...used to spread properties of an array/object
 
 //QUE 6
 const user = {name:'test',age:21}
 const admin = {admin:true,...user};
 console.log(admin);
-//output : it will add all the properties of user t admin object
+//output: it will add all the properties of user to admin object
 
 //QUE 7
-//what is destructig obj : taking out specific properties of objects
+//what is destructing obj: taking out specific properties of objects
 const testDestruct = {
     nameProperty:'test',
     fullname:{
@@ -132,13 +132,13 @@ let y;
 y=x 
 x.greeting = 'Hello';
 console.log(y.greeting)//Hello
-//we simply share the refernce not the properties of object 
-//as we changes the value of x.greeting and y has reference of X y's greeting will also change
+//we simply share the reference not the properties of the object 
+//as we change the value of x.greeting and y has a reference of X y's greeting will also change
 
 //QUE 10
 console.log({a:1}=={a:1}); //false
 console.log({b:1}== {b:1});// even if it === then also false
-// as each object has their own memory space
+// as each object has its own memory space
 
 //QUE 11
 let person = {name:'test'};
@@ -149,9 +149,9 @@ console.log(member);
 
 //QUE 11 
 //shallow copy and deep copy
-//shallow :  a new obj that we created shared the same reference of those surce obj
-// results in f you change either of them changes will  be occur t bth
-//DEEP:source and copy obj are completely independent | no refernce
+//shallow:  a new obj that we created shared the same reference of those source obj
+// results in f you change either of them changes will  be occur to both
+//DEEP: source and copy obj are completely independent | no reference
 
 const user2={
     name:'vaish',
