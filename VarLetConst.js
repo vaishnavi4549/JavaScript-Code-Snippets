c3a3d993fb107a1cfd7f73f5073f7b31a9a5dddc
Vaishnavi Kulkarni <55730110+vaishnavi4549@users.noreply.github.com>
2025-07-26 14:55:06 +0530

    Update VarLetConst.js

--- VarLetConst.js
+++ VarLetConst.js
@@ -1,10 +1,10 @@
 //Scope
 //block scope || global scope || function scope
 //var is function dcope 
-//let and const are block acope
+//let and const are block scope
 
 //variable shadowing
-//if try to shadow let with var it will be illigal shawing as let is block scope and trying to shaow shadow with var
+//if try to shadow let with var it will be illegal shadowing as let is block scope and trying to shadow shadow with var
 
 function test(){
     var a='hello';
@@ -19,27 +19,27 @@ function test(){
 test();
 
 //Declarations
-var a;// we can redeclare var as many times we want as it gets override new var with old one
-let a; // we cannot redecalre a variable using let in the same scope 
+var a;// we can redeclare var as many times as we want, as it gets overridden by a new var with the old one
+let a; // we cannot redeclare a variable using let in the same scope 
 const a=0; //same like cannot redeclare
 
 //Declaration without initialization
-var a; //allowed without inilization - takes undefined default
-let a; //allowed without inilization - takes undefined default 
-const a=0; // has to be initialized but can be reassigend a value to it
+var a; //allowed without initialization - takes undefined default
+let a; //allowed without initialization - takes undefined default 
+const a=0; // has to be initialized but can be reassigned a value to it
 
 
 //HOISTING
-//during the creation phase of Execution Context JS moves all the declaration to the top of program
+//during the creation phase of Execution Context, JS moves all the declarations to the top ofthe  program
 //called hoisting
 
-console.log(a); // it won't throw error as have undefined it got duing ceation phase 
+console.log(a); // it won't throw error as have undefined it got during creation phase 
 var a=1;
-//Let don't allow you to use variable before initialization
+//Let don't allow you to use a variable before initialization
 //Temporer dead zone - timing bet declaration & inilization of let & const variables
-//TDZ - contains all let & const which are in the scope but not yet initilized
-// let will be store in TDZ which will not be in global scope
-//LET nd CONST are hoisted in temporty Dead zone
+//TDZ - contains all let & const which are in the scope but not yet initialized
+// let will be stored in TD,Z, which will not be in the global scope
+//LET nd CONST are hoisted in temporary Dead zone
 console.log(b);// cannot access b before initialization
 let b=2;
 
