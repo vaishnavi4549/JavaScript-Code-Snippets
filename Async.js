0223e9d15fcda5fdd43fd7b944f201ba69a6780c
Vaishnavi Kulkarni <55730110+vaishnavi4549@users.noreply.github.com>
2025-12-17 19:39:52 +0530

    Update and rename Async to Async.js

--- Async.js
+++ Async.js
@@ -1,4 +1,4 @@
-Write a utility which prints numbers starting from an initial value and increment in steps which can be started and stopped by the user, any number of times
+//Write a utility which prints numbers starting from an initial value and increment in steps which can be started and stopped by the user, any number of times
 
 function Timer(initialvalue, endvalue){
     let value = initialvalue;
